52ce2de8f5c0e928ea2150d4db780e688814164b
caleb yates <caldog20@gmail.comz>
2024-03-03 19:05:24 -0600

    Cleanup

--- lib/stegan.go
+++ lib/stegan.go
@@ -61,18 +61,3 @@ func Encode(input string, output string, message string) error {
 	bufio.NewWriter(out).Write(encoded.Bytes())
 	return nil
 }
-
-// OpenImageFromPath returns a image.Image from a file path. A helper function to deal with decoding the image into a usable format. This method is optional.
-func OpenImageFromPath(filename string) (image.Image, error) {
-	inFile, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer inFile.Close()
-	reader := bufio.NewReader(inFile)
-	img, _, err := image.Decode(reader)
-	if err != nil {
-		return nil, err
-	}
-	return img, nil
-}
